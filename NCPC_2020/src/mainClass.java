6e0f3350c808f7dae37e8332cc1e310d19fa206a
Tamim365 <tamim.365.ti@gmail.com>
2020-09-30 04:05:29 +0600

    init

--- NCPC_2020/src/mainClass.java
+++ NCPC_2020/src/mainClass.java
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public class mainClass {
-
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
