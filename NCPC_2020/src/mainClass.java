9e494337612b2613321e9e6fa74dc1f3757bda59
Tamim365 <tamim.365.ti@gmail.com>
2020-09-30 05:17:13 +0600

    button_test

--- NCPC_2020/src/mainClass.java
+++ NCPC_2020/src/mainClass.java
@@ -1,5 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * 
+ * @author splash365 <tamim.365.ti at gmail.com>
+ */
 public class mainClass {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        HomeFrame home = new HomeFrame();
+        home.setVisible(true);
     }
 }
